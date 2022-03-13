be67a4ab15fcc49cd7472addecf5af49603d55eb
Nicolas <nicolas.huber2@uzh.ch>
2022-03-13 21:09:01 +0100

    avatar and description

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,5 +1,22 @@
 <template>
   <div>Me</div>
+  <div class="avatar">
+    <!-- Avatar -->
+    <img class="avatar__image" src="../assets/profile.jpg" />
+  </div>
+  <div class="card">
+    <!-- Cover -->
+    <div class="card__cover">
+      Nicolas Huber
+    </div>
+
+    <!-- Content -->
+    <div class="card__content">
+      Hallo Welt! Herzlich willkommen auf meiner Webseite. Mein Name ist Nicolas Huber und ich stelle mich hier gerne vor. Wer bin ich also?
+    </div>
+    Student und angehender Software-Enthusiast
+  </div>
+
 </template>
 
 <script>
@@ -10,4 +27,34 @@ export default {
 
 <style scoped>
 
+
+.avatar {
+  /* Rounded border */
+  border-radius: 50%;
+  height: 30%;
+  width: 30%;
+}
+
+.avatar__image {
+  /* Rounded border */
+  border-radius: 50%;
+
+  /* Take full size */
+  height: 100%;
+  width: 100%;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+}
+
+.card__cover {
+  height: 150px;
+  width: 100%;
+}
+
+.card__content {
+  /* Take available height */
+  flex: 1;
+}
 </style>
